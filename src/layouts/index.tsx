22eddbaa202fdf01d7f9d66584e1c5fe5386c792
Huang <1019118008@qq.com>
2021-07-28 17:21:45 +0800

    fix: 服务端的命令

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import { Layout, Menu, Drawer } from 'antd';
-import {CodeEditor} from '@/components';
-import 'codemirror/addon/search/search'
-import './style.less'
+import { CodeEditor } from '@/components';
+import './style.less';
 const { SubMenu } = Menu;
 const { Header } = Layout;
 
 const Layouts: React.FC = (props: any) => {
-  return <Layout className='global_layout' style={{ height: '100%' }}>
-    <Header className="header">
-      {/* <div className="logo" /> */}
-      <div className="logo">CodeZera</div>
-      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-      </Menu>
-    </Header>
-   {props.children}
-  </Layout>
-}
+  return (
+    <Layout className="global_layout" style={{ height: '100%' }}>
+      <Header className="header">
+        {/* <div className="logo" /> */}
+        <div className="logo">CodeZera</div>
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}></Menu>
+      </Header>
+      {props.children}
+    </Layout>
+  );
+};
 
 export default Layouts;
